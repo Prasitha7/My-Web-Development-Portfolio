2e0444164af25752713aa59700ce4da8a0953f7f
Prasitha7 <prasithajeevadya@gmail.com>
2023-12-18 17:23:24 +0530

    Added Logo Sequence and initiated links in index

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prasitha Samaraarachchi</title>
+    <link rel="stylesheet" href="frontend/styles/home-style.css">
+</head>
+<body>
+    <h2>Prasitha samaraarachchi</h2>
+    <canvas class="canvas"></canvas>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.4/gsap.min.js" integrity="sha512-EZI2cBcGPnmR89wTgVnN3602Yyi7muWo8y1B3a8WmIv1J9tYG+udH4LvmYjLiGp37yHB7FfaPBo8ly178m9g4Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.4/ScrollTrigger.min.js" integrity="sha512-OzC82YiH3UmMMs6Ydd9f2i7mS+UFL5f977iIoJ6oy07AJT+ePds9QOEtqXztSH29Nzua59fYS36knmMcv79GOg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="frontend/js/background.js"></script>
+    <a href="signin-view.php">Sign in</a>
+    <a href="login-view.php">Login</a>
+</body>
+</html>
