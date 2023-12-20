66daf118f831a230b57073f9394016ce2118ad44
Prasitha7 <prasithajeevadya@gmail.com>
2023-12-20 12:51:31 +0530

    Image Sequence not loading fix

--- frontend/js/background.js
+++ frontend/js/background.js
@@ -5,7 +5,7 @@ canvas.height = window.innerHeight;
 const context = canvas.getContext("2d");
 const frameCount = 30;
 
-const currentFrame = (index) =>`assets/sequences/pj logo sequ 15fps/${(index+1).toString()}.png`;
+const currentFrame = (index) => `./assets/sequences/pj logo sequ 15fps/${(index+1).toString()}.png`;
 const images = [];
 let pj = {frame : 0}
 
@@ -15,9 +15,22 @@ for(let i = 0; i<frameCount; i++){
     images.push(img)
 }
 
+gsap.to(pj,{
+        frame:frameCount-1,
+        snap:"frame",
+        ease:"none",
+        scrollTrigger:{
+            scrub: true,
+            pin:"canvas",
+            end:"500%",
+        },
+        onUpdate: render,
+    }
+)
+
 images[0].onload = render;
 
 function render(){
-    context.clearRect(0,0,canvas.width, canvas.height);
+    context.clearRect(0,0, canvas.width, canvas.height);
     context.drawImage(images[pj.frame], 0, 0);
 }
