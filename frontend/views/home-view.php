e5a7709b1a2486eda0628c9a17aadd960426ed70
Prasitha7 <prasithajeevadya@gmail.com>
2023-12-27 11:32:51 +0530

    navigation bar stylising

--- frontend/views/home-view.php
+++ frontend/views/home-view.php
@@ -4,13 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!--Font Awsom-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!--Google Fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!--Inria Font-->
+    <link href="https://fonts.googleapis.com/css2?family=Inria+Sans&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../styles/home-style.css">
     <link rel="stylesheet" href="../styles/navbar-style.css">
 </head>
 <body>
+
+<!--Navigation Bar-->
 <div class="topnav">
     <div class="topnav-menu">
-        <a class="active" href="#home">I am PrasithaJ</a>
+        <a class="home" href="home-view.php">I am PrasithaJ</a>
         <a href="#news">About</a>
         <a href="#portfolio">Portfolio</a>
         <a href="#skills">Skills</a> 
@@ -18,13 +29,14 @@
     </div>
 
 
-  <!-- Add login and sign-in -->
+    <!-- Add login and sign-in -->
     <div class="topnav-right">
-        <a href="#login">Login</a>
-        <a href="#signin">Sign In</a>
+        <a href="login-view.php">Login</a>
+        <a href="signin-view.php">Sign In</a>
     </div>
 </div>
 
+<!--Website Start-->
 <div class="hero">
     <h3>Showcasing</h3>
     <h1>my <br> Web Development <br> Skills</h1>
